f6f2a81753e4308496ad74ada7dcc50f8770799a
sombo08 <55875872+sombo08@users.noreply.github.com>
2024-03-30 14:15:40 +0200

    Update UnitConverter.js

--- UnitConverter.js
+++ UnitConverter.js
@@ -1,23 +1,20 @@
 let unit = 'км';
-let value = 2;
+let value = 23;
 
-function convertUnit(unit, value) {
-    let result;
+let result;
 
-    switch (unit) {
-        case 'км':
-            result = value * 1000 + ' м';
-            break;
-        case 'год':
-            result = value * 60 + ' хв';
-            break;
-        case 'кг':
-            result = value * 1000 + ' г';
-            break;
-        default:
-            result = 'Невідома одиниця виміру';
+switch (unit) {
+    case 'км':
+        result = value * 1000 + ' м';
+        break;
+    case 'год':
+        result = value * 60 + ' хв';
+        break;
+    case 'кг':
+        result = value * 1000 + ' г';
+        break;
+    default:
+        result = 'Невідома одиниця виміру';
     }
-    return result;
-}
 
-console.log(value + ' ' + unit + ' це ' + convertUnit(unit, value));
+console.log(value + ' ' + unit + ' це ' + result);
