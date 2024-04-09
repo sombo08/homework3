99c4cfe6c403d0c68ef26d3887b713773fefa888
sombo08 <55875872+sombo08@users.noreply.github.com>
2024-04-09 22:30:20 +0300

    Add files via upload

---
+++ Company.js
@@ -0,0 +1,66 @@
+const company = { 
+   name: 'Велика Компанія',
+    type:'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+        {
+            name: 'Клієнт 1',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+                {
+                    name: 'Клієнт 1.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                },
+                {
+                    name: 'Клієнт 1.2',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                    partners: [
+                        {
+                            name: 'Клієнт 1.2.3',
+                            type: 'subSubCompany',
+                            uses: 'Рішення для продажу квитків',
+                            sells: 'Рішення для продажу квитків',
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Клієнт 2',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків'
+        }
+    ]
+};
+
+function findValueByKey(companyName) {
+    const client = company.clients.find(client => client.name === companyName);
+    if (client) {
+        return {
+            name: client.name,
+            type: client.type,
+            uses: client.uses,
+            sells: client.sells,
+            partners: client.partners || []
+        };
+    } else {
+        return null;
+    }
+}
+
+const companyName = 'Клієнт 1';
+const companyInfo = findValueByKey(companyName);
+if (companyInfo) {
+    console.log(`Інформація про ${companyName}:`);
+    console.log(companyInfo);
+} else {
+    console.log(`${companyName} не знайдено.`);
+}
